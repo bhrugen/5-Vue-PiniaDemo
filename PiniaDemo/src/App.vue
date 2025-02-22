f3a588ffa26f8dc4382f565e5541c0a5c5207ab9
Bhrugen Patel <bhrugen1990@gmail.com>
2025-02-22 17:48:58 -0500

    Section 16 - Create Project

---
+++ PiniaDemo/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">Hello World</div>
+  </header>
+
+  <RouterView />
+</template>
