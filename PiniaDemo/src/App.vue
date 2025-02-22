040091e8ba00a829171ba3f9e9007f135bab0f19
Bhrugen Patel <bhrugen1990@gmail.com>
2025-02-22 17:51:56 -0500

    Section 16 - Routing in action

--- PiniaDemo/src/App.vue
+++ PiniaDemo/src/App.vue
@@ -3,9 +3,35 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">Hello World</div>
-  </header>
-
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">Pinia</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/info"
+              >Info</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <RouterView />
 </template>
